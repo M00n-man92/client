d838de41f39cfd08a0543b1b1214e54434f5c590
M00n-man92 <azraelnumb00@gmail.com>
2022-03-20 05:42:57 +0300

    this what i cal karma this shit must've had me goin crazy but learned al ot soo

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -89,7 +89,7 @@ export default function Message({ own }) {
     useEffect(() => {
         socket.current.emit("addUser", id)
         // console.log(socket)
-        socket.current.on("getUsers", users => { console.log(users) })
+        socket.current.on("getUsers")
     }, [id])
 
     useEffect(() => {
