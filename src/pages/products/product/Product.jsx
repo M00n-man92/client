953b5d36da9093084967fa661e3db3546c811bff
M00n-man92 <azraelnumb00@gmail.com>
2022-03-27 16:56:41 +0300

    adding padding og 1 em at least the ;ayout looks nice

--- src/pages/products/product/Product.jsx
+++ src/pages/products/product/Product.jsx
@@ -2,18 +2,18 @@ import { FavoriteBorderOutlined, ShoppingBagOutlined } from '@mui/icons-material
 import { Link } from 'react-router-dom'
 import './product.scss'
 
-export default function Product({ items,toss }) {
-//   console.log(toss)
+export default function Product({ items, toss }) {
+    //   console.log(toss)
     return (
         <div className="product" key={toss}>
-            <div className="circle"></div>
-            <img src={items.img} alt="" /> 
-             
-               <div className="display">
+            {/* <div className="circle"></div> */}
+            <img src={items.img} alt="" />
+
+            <div className="display">
                 <FavoriteBorderOutlined className="icon" />
                 <Link className="link" to={`/product/${items._id}`}><ShoppingBagOutlined className="icon" /></Link>
-                
-                </div>
+
+            </div>
             <div className="price">
                 <span>$ {items.price}</span>
             </div>
