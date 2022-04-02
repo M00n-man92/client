b8960ff85d00f5180d1c7e5989f08def28cae3dd
M00n-man92 <azraelnumb00@gmail.com>
2022-04-02 04:47:13 +0300

    started makeing the home sctten more attractive working on the sliders right now

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
             <Announcemets />
             <Nav />
             {/* <Catagories/> */}
-            <div className="sexcatagories">
+            {/* <div className="sexcatagories">
                 <Link to="/products/men" className='link'>
                 <div className="makecatagory" >
                     <MaleOutlined />
@@ -31,10 +31,98 @@ export default function Home() {
                         WOMEN
                     </span>
 
-                </div>
+                </div> 
                 </Link>
+            </div> */}
+            {/* <Products /> */}
+            <div className="beautify">
+                <div className="companyname">
+                    <img src="/assets/laser.webp" alt="" />
+                </div>
+                <div className="sliderorproduct">
+                <div className="dopemoney">
+                    <div className="keepmoving">
+                    
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                        <div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div><div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div><div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div><div className="singleproduct">
+                            {/* <img src="/assets/1625808057956735e5accb327524affc0e11d1fd12_thumbnail_405x552.webp" alt="" /> */}
+                            <div className="priceofproduct">
+                            <span>345</span>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    
+                </div>
             </div>
-            <Products />
             <Footer />
             {/* <Menu /> */}
         </div>
