f27964a511901c7f0e43d5cdff54671a2e58a85a
M00n-man92 <azraelnumb00@gmail.com>
2022-12-07 11:26:50 +0300

    alterations to some of the pages for the mobile version

--- src/pages/newpages/newproducts/Newproducts.jsx
+++ src/pages/newpages/newproducts/Newproducts.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./newproducts.scss"
 import Products from '../../products/Products'
 import Product from '../../products/product/Product'
+import { fontWeight } from '@mui/system'
 export default function NewProducts() {
+  const nav =["Hot", "On SALE","Trending Now", "New Arrival"]
+  const navPointer = [0,1,2,3];
+  const [isLined, setIsLined] = useState(0);
   return (
     
     <div className='newproducts'>
       <h3>Our Product</h3>
       <div className="container">
-        <span>Hot</span>
-        <span>On SALE</span>
-        <span>Trending Now</span>
-        <span>New Arrival</span>
+        {nav.map((item,index)=>(
+          <span onClick={(e)=>{setIsLined(index)}} key={index} style= {index === isLined?{ textDecorationLine: "underline", fontWeight:"Bold" , textDecorationThickness:"10%"}:{}}>{item}</span>
+        ))}
       </div>
       <div className="productscontainer">
       <Products sex="women"/>
