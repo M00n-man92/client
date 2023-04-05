f724a4c3339ce940c31b1f07bcf994124cdb8a93
M00n-man92 <azraelnumb00@gmail.com>
2023-04-05 20:25:27 +0300

    made ths site into a pic

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,27 +1,25 @@
-
-import Footer from '../footer/Footer'
-import Nav from '../nav/Navigation'
-import Into from '../newpages/begining/Into'
+import Footer from "../footer/Footer"
+import Nav from "../nav/Navigation"
+import Into from "../newpages/begining/Into"
 import Second from "../newpages/second/Second"
 import Third from "../newpages/three/Third"
 import Fourth from "../newpages/four/Fourth"
 import Timer from "../newpages/timer/Timer"
-import './home.scss'
+import "./home.scss"
 // import { MaleOutlined, FemaleOutlined, ArrowForwardIosOutlined, ArrowBackIosNewOutlined } from '@mui/icons-material'
 // import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { useState } from "react"
 // import { Link } from 'react-router-dom'
 // import { publicRequest } from '../../requestMethods'
 // import Product from '../products/product/Product'
-import Fifith from '../newpages/five/Fifith'
-import Comments from '../newpages/comments/Comments'
-import Newproducts from '../newpages/newproducts/Newproducts'
+import Fifith from "../newpages/five/Fifith"
+import Comments from "../newpages/comments/Comments"
+import Newproducts from "../newpages/newproducts/Newproducts"
 export default function Home() {
+  // const [productm, setProductm] = useState([])
+  // const [itemm, setitemm] = useState(0)
 
-	// const [productm, setProductm] = useState([])
-	// const [itemm, setitemm] = useState(0)
-
-	/* const nion = (digits) => {
+  /* const nion = (digits) => {
 		if (digits == "l") {
 			itemm !== 0 ? setitemm(itemm - 1) : setitemm(2)
 			// console.log(itemm)
@@ -31,29 +29,29 @@ export default function Home() {
 			console.log(itemm)
 		}
 	} */
-	// console.log(itemm)
-	/* useEffect(() => {
+  // console.log(itemm)
+  /* useEffect(() => {
 		const stuallday = async () => {
 			const answer = await publicRequest.get('/product/find/limit/home')
 			setProductm(answer.data.data)
 		}
 		stuallday()
 	}, []) */
-	return (
-		<div className="home">
-			<Nav />
-			<div className="beautify">
-				<Into />
-				<Second />
+  return (
+    <div className="home">
+      {/* <Nav /> */}
+      <div className="beautify">
+        <Into />
+        {/* <Second />
 				<Third />
 				<Fourth />
 				<Newproducts />
 				<Timer />
 				<Fifith />
-				<Comments />
-			</div>
-			<Footer />
-			{/* <Menu /> */}
-		</div>
-	)
+				<Comments /> */}
+      </div>
+      {/* <Footer /> */}
+      {/* <Menu /> */}
+    </div>
+  )
 }
--- src/pages/newpages/begining/Into.jsx
+++ src/pages/newpages/begining/Into.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
+import React from "react"
 import "./intro.scss"
 //  "node-sass": "^6.0.1",
 // "node-sass": "^7.0.3",
 export default function Into() {
   return (
     <div className="intro">
-      <div className="container">
-        <div className="header">
-          <h3>Discover your ideal Ethiopian fashion look.</h3>
-        </div>
-        <div className="mid">
-          <span>
-          here at The Ethiopian Fashion Look you will find all kinds of styles from head to toe
-          </span>
-        </div>
-        <div className="last">
-          <button>SHOP NOW</button>
-        </div>
-      </div>
-      <div className="circle">
+      <img
+        src="/assets/coming soon 10.png"
+        alt=""
+        style={{ width: "100dvw", height: "100dvh", objectFit: "cover" }}
+      />
+      {/* <div className="container"> */}
+      {/*      <div className="header">
+           <h3>Discover your ideal Ethiopian fashion look.</h3>
+         </div>
+         <div className="mid">
+           <span>
+           here at The Ethiopian Fashion Look you will find all kinds of styles from head to toe
+           </span>
+         </div>
+         <div className="last">
+           <button>SHOP NOW</button>
+         </div>
+       </div>
+       <div className="circle">
 
-      </div>
-      <div className="picture">
-        <img src="./assets/Gojjam Amhara.jfif" alt="" />
-      </div>
+       </div>
+       <div className="picture">
+         <img src="./assets/Gojjam Amhara.jfif" alt="" />
+       </div> */}
     </div>
   )
 }
